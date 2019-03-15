82c6bea5219b35077744ab1b7d202f9f3c00c760
Dara <43659748+DaraUng@users.noreply.github.com>
2019-03-14 19:15:01 -0700

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 yum -y install python-pip python-devel gcc postgresql-server postgresql-devel postgresql-contrib
-sudo postgresql-setup initdb
+postgresql-setup initdb
 systemctl start postgresql
 vim /var/lib/pgsql/data/pg_hba.conf
 sed -i 's,host    all             all             127.0.0.1/32            ident,host    all             all             127.0.0.1/32            md5,g' /var/lib/pgsql/data/pg_hba.conf
 sed -i 's,host    all             all             ::1/128                 ident,host    all             all             ::1/128                 md5,g' /var/lib/pgsql/data/pg_hba.conf
 systemctl restart postgresql
 systemctl enable postgresql
-
-
-
 
 echo "CREATE DATABASE myproject;
 CREATE USER myprojectuser WITH PASSWORD 'password';
@@ -36,7 +33,7 @@ GRANT ALL PRIVILEGES ON DATABASE mypgdb TO pgdbuser;" > /tmp/phpmyadmin
 sed -i 's/Require local/Require all granted/g' /etc/httpd/conf.d/phpPgAdmin.conf
 sed -i 's/Denay from all/Allow all/g' /etc/httpd/conf.d/phpPgAdmin.conf
 
-systemctl reload httpd.service
+systemctl restart httpd
 systemctl restart postgresql
 sudo systemctl restart 
 
